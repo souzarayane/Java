7114a17d015dc7a3efc2c310aca00f06ef446823
Rayane Souza <73654615+souzarayane@users.noreply.github.com>
2022-03-19 10:57:43 -0300

    Create While02.java

---
+++ Introdução/estruturaDeControle/While02.java
@@ -0,0 +1,21 @@
+package controle;
+
+public class While02 {
+
+	public static void main(String[] args) {
+		
+		int idade = 10;
+		
+		while(idade < 18) {
+			System.out.println("Idade " + idade);
+			idade += 1;
+		}
+
+		int i = 0;
+		while(i < 10) {
+			System.out.println("Contador " + i);
+			i += 1;
+		}
+	}
+
+}
